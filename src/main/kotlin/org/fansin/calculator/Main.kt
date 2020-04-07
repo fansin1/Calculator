c658fa4324197775f779676ebe255eda1b959c3b
Fansin <artem21481@gmail.com>
2020-04-08 00:09:07 +0300

    add plus operation

--- src/main/kotlin/org/fansin/calculator/Main.kt
+++ src/main/kotlin/org/fansin/calculator/Main.kt
@@ -1,9 +1,11 @@
 package org.fansin.calculator
 
-import org.fansin.calculator.operation.Operation
+import org.fansin.calculator.operation.PlusOperation
 
 fun main () {
-    val operations = hashMapOf<String, Operation>()
+    val operations = hashMapOf(
+        "+" to PlusOperation()
+    )
     CalculatorConsole(
         System.`in`.bufferedReader(), System.out.writer(),
         Calculator(operations)
