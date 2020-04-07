9bdf425496e5de1b25f9b36c7242ec1d1aea8a3f
Fansin <artem21481@gmail.com>
2020-04-08 01:36:20 +0300

    add multiply operation

--- src/main/kotlin/org/fansin/calculator/Main.kt
+++ src/main/kotlin/org/fansin/calculator/Main.kt
@@ -2,13 +2,15 @@ package org.fansin.calculator
 
 import org.fansin.calculator.operation.DivideOperation
 import org.fansin.calculator.operation.MinusOperation
+import org.fansin.calculator.operation.MultiplyOperation
 import org.fansin.calculator.operation.PlusOperation
 
 fun main () {
     val operations = hashMapOf(
         "+" to PlusOperation(),
         "-" to MinusOperation(),
-        "/" to DivideOperation()
+        "/" to DivideOperation(),
+        "*" to MultiplyOperation()
     )
     CalculatorConsole(
         System.`in`.bufferedReader(), System.out.writer(),
---
+++ src/main/kotlin/org/fansin/calculator/operation/MultiplyOperation.kt
@@ -0,0 +1,5 @@
+package org.fansin.calculator.operation
+
+class MultiplyOperation : Operation {
+    override fun execute(num1: Long, num2: Long) = num1 * num2
+}
